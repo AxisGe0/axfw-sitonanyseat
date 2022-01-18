7a8f3e857c2bd9bb69fed0680235b5fbc4cf2767
AxisGe0 <69110100+AxisGe0@users.noreply.github.com>
2022-01-18 21:14:14 +0530

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,33 +1,49 @@
+Settings = {
+    KeepEngineOn = true ---Keeps The engine on after leaving the vehicle if the engine is on,
+    NPCCheck = true --- Adds NPC Check to the code(Checks if there is any ped inside vehicle or not)
+}
+
 CreateThread(function()
-    local dist,index
+    local dist, index
     while true do
         local ped = GetPlayerPed(-1)
-        if IsControlJustPressed(0,75) then 
-            if IsPedInAnyVehicle(ped) then 
-                local veh = GetVehiclePedIsIn(ped)
-                if GetIsVehicleEngineRunning(veh) then
-                    TaskLeaveVehicle(ped, veh, 0)
-                    Wait(1000)
-                    SetVehicleEngineOn(veh, true, true, true)
+        if IsControlJustPressed(0, 75) then
+            if IsPedInAnyVehicle(ped) then
+                if Settings.KeepEngineOn then
+                    local veh = GetVehiclePedIsIn(ped)
+                    if GetIsVehicleEngineRunning(veh) then
+                        TaskLeaveVehicle(ped, veh, 0)
+                        Wait(1000)
+                        SetVehicleEngineOn(veh, true, true, true)
+                    end
                 end
-            else 
+            else
                 local veh = GetVehiclePedIsTryingToEnter(ped)
-                if veh ~= 0 then 
-                    local coords = GetEntityCoords(ped)
-                    if #(coords-GetEntityCoords(veh)) <= 3.5 then
-                        ClearPedTasks(ped)
-                        ClearPedSecondaryTask(ped)
-                        for i=0,GetNumberOfVehicleDoors(veh),1 do 
-                            local coord = GetEntryPositionOfDoor(veh, i)
-                            if (IsVehicleSeatFree(veh,i-1) and GetVehicleDoorLockStatus(veh) ~= 2) then
-                                if dist == nil then dist = #(coords-coord);index = i end 
-                                if #(coords-coord) < dist then dist = #(coords-coord);index = i end
+                if veh ~= 0 then
+                    if CanSit(veh) then
+                        local coords = GetEntityCoords(ped)
+                        if #(coords - GetEntityCoords(veh)) <= 3.5 then
+                            ClearPedTasks(ped)
+                            ClearPedSecondaryTask(ped)
+                            for i = 0, GetNumberOfVehicleDoors(veh), 1 do
+                                local coord = GetEntryPositionOfDoor(veh, i)
+                                if (IsVehicleSeatFree(veh, i - 1) and
+                                    GetVehicleDoorLockStatus(veh) ~= 2) then
+                                    if dist == nil then
+                                        dist = #(coords - coord)
+                                        index = i
+                                    end
+                                    if #(coords - coord) < dist then
+                                        dist = #(coords - coord)
+                                        index = i
+                                    end
+                                end
                             end
-                        end 
-                        if index then
-                            TaskEnterVehicle(ped, veh, 10000, index - 1, 1.0, 1, 0) 
+                            if index then
+                                TaskEnterVehicle(ped, veh, 10000, index - 1,1.0, 1, 0)
+                            end
+                            index, dist = nil, nil
                         end
-                        index,dist = nil,nil
                     end
                 end
             end
@@ -35,3 +51,13 @@ CreateThread(function()
         Wait(1)
     end
 end)
+
+CanSit = function(veh)
+    if not Settings.NPCCheck then 
+        return true 
+    end
+    for i = -1, 15 do
+        if IsEntityAPed(GetPedInVehicleSeat(veh, i)) then return false end
+    end
+    return true
+end
